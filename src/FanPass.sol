2a596d8f630aff4366cc86459e5f1c0336dd0511
Alejo Ezequiel Vilches <alejoevilches@gmail.com>
2026-02-05 20:13:29 -0300

    add bbuytickets feature

--- src/FanPass.sol
+++ src/FanPass.sol
@@ -5,6 +5,7 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 struct Pass {
     uint256 eventsAttended;
+    mapping(uint256 => bool) attendedEvents;
 }
 
 contract FanPass is ERC721 {
@@ -12,6 +13,8 @@ contract FanPass is ERC721 {
     uint256 passCount;
 
     error MintFanPass_PassAlreadyMinted();
+    error BuyTicket_NoFanPass();
+    error BuyTicket_NotAuthorized();
 
     constructor() ERC721("FanPass", "FNP") {}
 
@@ -19,5 +22,13 @@ contract FanPass is ERC721 {
         if (balanceOf(msg.sender) > 0) revert MintFanPass_PassAlreadyMinted();
         _safeMint(msg.sender, passCount);
         passCount++;
+    }
+
+    function buyTicket(uint256 eventId, uint256 eventsAttendedToBuy) external {
+        if (balanceOf(msg.sender) < 0) revert BuyTicket_NoFanPass();
+        if (addressToFanPass[msg.sender].eventsAttended < eventsAttendedToBuy)
+            revert BuyTicket_NotAuthorized();
+        addressToFanPass[msg.sender].eventsAttended++;
+        addressToFanPass[msg.sender].attendedEvents[eventId] = true;
     }
 }
