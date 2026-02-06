44aa790b4fbf9ef34cd6d6a588cd856fe747060b
Alejo Ezequiel Vilches <alejoevilches@gmail.com>
2026-02-06 14:53:51 -0300

    start buy ticket feature

--- src/FanPass.sol
+++ src/FanPass.sol
@@ -16,14 +16,14 @@ struct Tier {
 }
 
 struct Pass {
-    uint256 eventsAttended;
+    uint256 eventsCount;
     mapping(uint256 => bool) attendedEvents;
 }
 
 struct Event {
     STATUS status;
     uint256 requiredEventId;
-    uint256 eventAttendedToBuy;
+    uint256 eventsAttendedToBuy;
     uint256 tierCount;
     mapping(uint256 => Tier) tierCountToTier;
 }
@@ -42,6 +42,9 @@ contract FanPass is ERC721 {
     error MintFanPass_PassAlreadyMinted();
     error BuyTicket_NoFanPass();
     error BuyTicket_NotAuthorized();
+    error BuyTicket_InvalidTier();
+    error BuyTicket_NoTicketsAvailable();
+    error BuyTicket_NotEnoughEth();
     error OnlyOwner();
 
     constructor() ERC721("FanPass", "FNP") {
@@ -57,16 +60,40 @@ contract FanPass is ERC721 {
     function createEvent(
         uint256 eventId,
         uint256 requiredEventId,
-        uint256 eventAttendedToBuy
+        uint256 eventsAttendedToBuy
     ) external view onlyOwner {
         idToEvent[eventId].status == STATUS.ACTIVE;
         idToEvent[eventId].requiredEventId == requiredEventId;
-        idToEvent[eventId].eventAttendedToBuy == eventAttendedToBuy;
+        idToEvent[eventId].eventsAttendedToBuy == eventsAttendedToBuy;
     }
 
     function createTier(uint256 eventId, Tier memory tier) internal onlyOwner {
         uint256 count = idToEvent[eventId].tierCount;
         idToEvent[eventId].tierCountToTier[count] = tier;
         idToEvent[eventId].tierCount++;
+    }
+
+    function buyTickets(
+        uint256 eventId,
+        uint256 amount,
+        uint256 tier
+    ) public payable {
+        if (balanceOf(msg.sender) < 0) revert BuyTicket_NoFanPass();
+        uint256 assistanceFilter = idToEvent[eventId].eventsAttendedToBuy;
+        if (addressToFanPass[msg.sender].eventsCount < assistanceFilter)
+            revert BuyTicket_NotAuthorized();
+        if (tier >= idToEvent[eventId].tierCount)
+            revert BuyTicket_InvalidTier();
+        if (idToEvent[eventId].tierCountToTier[tier].amount < amount)
+            revert BuyTicket_NoTicketsAvailable();
+        uint256 amountRequired = idToEvent[eventId]
+            .tierCountToTier[tier]
+            .price * amount;
+        if (msg.value < amountRequired) revert BuyTicket_NotEnoughEth();
+        addressToFanPass[msg.sender].eventsCount++;
+        addressToFanPass[msg.sender].attendedEvents[eventId] = true;
+        idToEvent[eventId].tierCountToTier[tier].amount =
+            idToEvent[eventId].tierCountToTier[tier].amount -
+            amount;
     }
 }
